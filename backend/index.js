156473aa8bcd587b069fc72027e2aeb4fa4b6fdf
Shresth Singh <shresthengineer@gmail.com>
2026-06-24 10:55:51 +0530

    entered log to test the error

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const app = require("./src/app");
 const dbconnect = require("./src/config/db");
 
+console.log("enter");
 dbconnect();
+console.log("exit");
 
 export default app;
